51d3d5636e5c6245c01e96cfb7676565058c45c1
Elena Karpenko <shtvlnk@gmail.com>
2022-12-28 19:29:15 +0700

    stack content update

--- front.dev/sections/Stack/constants.ts
+++ front.dev/sections/Stack/constants.ts
@@ -1,4 +1,4 @@
-import golandImg from "@assets/img/goland.png";
+import golangImg from "@assets/img/goland.png";
 import nodejsImg from "@assets/img/nodejs.png";
 import pythonImg from "@assets/img/python.png";
 import phpImg from "@assets/img/php.png";
@@ -9,12 +9,49 @@ import reactJsImg from "@assets/img/reactJs.png";
 import swiftImg from "@assets/img/swift.png";
 import kotlinImg from "@assets/img/kotlin.png";
 import sparkImg from "@assets/img/spark.png";
-import googleCloudImg from "@assets/img/googleCloud.png";
 import hiveImg from "@assets/img/hive.png";
 import prestoImg from "@assets/img/presto.png";
 import scalaImg from "@assets/img/scala.png";
 import appiumImg from "@assets/img/appium.png";
 import vueImg from "@assets/img/vue.png";
+import kafkaImg from "@assets/img/kafka.png";
+import ydbImg from "@assets/img/ydb.png";
+import swaggerImg from "@assets/img/swagger.png";
+import grpcImg from "@assets/img/grpc.png";
+import snapkitImg from "@assets/img/snapkit.png";
+import xcImg from "@assets/img/xc.png";
+import alamofireImg from "@assets/img/alamofire.png";
+import swinjectImg from "@assets/img/swinject.png";
+import udfImg from "@assets/img/udf.png";
+import daggerImg from "@assets/img/dagger.png";
+import retrofitImg from "@assets/img/retrofit.png";
+import rxjavaImg from "@assets/img/rxjava.png";
+import coroutinesImg from "@assets/img/coroutines.png";
+import coilImg from "@assets/img/coil.png";
+import ciceroneImg from "@assets/img/cicerone.png";
+import awsImg from "@assets/img/aws.png";
+import gcpImg from "@assets/img/gcp.png";
+import helmImg from "@assets/img/helm.png";
+import terraformImg from "@assets/img/terraform.png";
+import woodpeckerImg from "@assets/img/woodpecker.png";
+import apolloGqlImg from "@assets/img/apolloGql.png";
+import typescriptImg from "@assets/img/typescript.png";
+import nextJsImg from "@assets/img/nextJs.png";
+import bigqueryImg from "@assets/img/bigquery.png";
+import clickhouseImg from "@assets/img/clickhouse.png";
+import trinoImg from "@assets/img/trino.png";
+import icebergImg from "@assets/img/iceberg.png";
+import airflowImg from "@assets/img/airflow.png";
+import scipyImg from "@assets/img/scipy.png";
+import scikitLearnImg from "@assets/img/scikitLearn.png";
+import opencvImg from "@assets/img/opencv.png";
+import catboostImg from "@assets/img/catboost.png";
+import pytorchImg from "@assets/img/pytorch.png";
+import kubeflowImg from "@assets/img/kubeflow.png";
+import qaseImg from "@assets/img/qase.png";
+import proxymanImg from "@assets/img/proxyman.png";
+import postmanImg from "@assets/img/postman.png";
+import allureImg from "@assets/img/allure.png";
 
 import { getRandomId } from "@utils/shared";
 
@@ -23,60 +60,44 @@ import { TSlideData } from "./components/Slider";
 export const sliderData: Array<TSlideData> = [
     {
         id: getRandomId(),
-        label: "Backend & Infra",
+        label: "Backend",
         lang: [
             {
                 id: getRandomId(),
-                img: golandImg,
-                label: "goland",
-            },
-            {
-                id: getRandomId(),
-                img: nodejsImg,
-                label: "nodejs",
-            },
-            {
-                id: getRandomId(),
-                img: pythonImg,
-                label: "python",
-            },
-            {
-                id: getRandomId(),
-                img: phpImg,
-                label: "php",
+                img: golangImg,
+                label: "GOLANG",
             },
             {
                 id: getRandomId(),
                 img: mysqlImg,
-                label: "mysql",
+                label: "MYSQL",
             },
             {
                 id: getRandomId(),
                 img: redisImg,
-                label: "redis",
+                label: "REDIS",
             },
             {
                 id: getRandomId(),
-                img: kubernetesImg,
-                label: "kubernetes",
-            },
-        ],
-    },
-    {
-        id: getRandomId(),
-        label: "Web",
-        lang: [
-            {
-                id: getRandomId(),
-                img: reactJsImg,
-                label: "React JS",
+                img: kafkaImg,
+                label: "KAFKA",
             },
             {
                 id: getRandomId(),
-                img: null,
-                label: "Microfrontends",
+                img: ydbImg,
+                label: "YDB",
             },
-        ],
+            {
+                id: getRandomId(),
+                img: swaggerImg,
+                label: "SWAGGER",
+            },
+            {
+                id: getRandomId(),
+                img: grpcImg,
+                label: "GRPC",
+            }
+        ]
     },
     {
         id: getRandomId(),
@@ -85,14 +106,34 @@ export const sliderData: Array<TSlideData> = [
             {
                 id: getRandomId(),
                 img: swiftImg,
-                label: "Swift",
+                label: "SWIFT",
             },
             {
                 id: getRandomId(),
-                img: null,
-                label: "User-Defined Functions",
+                img: snapkitImg,
+                label: "SNAPKIT",
             },
-        ],
+            {
+                id: getRandomId(),
+                img: xcImg,
+                label: "XCOORDINATOR",
+            },
+            {
+                id: getRandomId(),
+                img: alamofireImg,
+                label: "ALAMOFIRE",
+            },
+            {
+                id: getRandomId(),
+                img: swinjectImg,
+                label: "SWINJECT",
+            },
+            {
+                id: getRandomId(),
+                img: udfImg,
+                label: "UNIDIRECTIONAL DATA FLOW",
+            }
+        ]
     },
     {
         id: getRandomId(),
@@ -101,14 +142,116 @@ export const sliderData: Array<TSlideData> = [
             {
                 id: getRandomId(),
                 img: kotlinImg,
-                label: "Kotlin",
+                label: "KOTLIN",
             },
             {
                 id: getRandomId(),
-                img: null,
-                label: "MVVM/MVI",
+                img: daggerImg,
+                label: "DAGGER",
             },
-        ],
+            {
+                id: getRandomId(),
+                img: retrofitImg,
+                label: "RETROFIT",
+            },
+            {
+                id: getRandomId(),
+                img: rxjavaImg,
+                label: "RXJAVA",
+            },
+            {
+                id: getRandomId(),
+                img: coroutinesImg,
+                label: "COROUTINES",
+            },
+            {
+                id: getRandomId(),
+                img: coilImg,
+                label: "COIL",
+            },
+            {
+                id: getRandomId(),
+                img: ciceroneImg,
+                label: "CICERONE",
+            }
+        ]
+    },
+    {
+        id: getRandomId(),
+        label: "Infrastructure",
+        lang: [
+            {
+                id: getRandomId(),
+                img: awsImg,
+                label: "AWS",
+            },
+            {
+                id: getRandomId(),
+                img: gcpImg,
+                label: "GCP",
+            },
+            {
+                id: getRandomId(),
+                img: kubernetesImg,
+                label: "KUBERNETES",
+            },
+            {
+                id: getRandomId(),
+                img: helmImg,
+                label: "HELM",
+            },
+            {
+                id: getRandomId(),
+                img: terraformImg,
+                label: "TERRAFORM",
+            },
+            {
+                id: getRandomId(),
+                img: woodpeckerImg,
+                label: "WOODPECKER",
+            },
+            {
+                id: getRandomId(),
+                img: mysqlImg,
+                label: "MYSQL",
+            },
+            {
+                id: getRandomId(),
+                img: redisImg,
+                label: "REDIS",
+            },
+            {
+                id: getRandomId(),
+                img: kafkaImg,
+                label: "KAFKA",
+            }
+        ]
+    },
+    {
+        id: getRandomId(),
+        label: "Web",
+        lang: [
+            {
+                id: getRandomId(),
+                img: reactJsImg,
+                label: "REACT JS",
+            },
+            {
+                id: getRandomId(),
+                img: apolloGqlImg,
+                label: "APOLLO GQL",
+            },
+            {
+                id: getRandomId(),
+                img: typescriptImg,
+                label: "TYPESCRIPT",
+            },
+            {
+                id: getRandomId(),
+                img: nextJsImg,
+                label: "NEXT.JS",
+            }
+        ]
     },
     {
         id: getRandomId(),
@@ -116,35 +259,111 @@ export const sliderData: Array<TSlideData> = [
         lang: [
             {
                 id: getRandomId(),
-                img: sparkImg,
-                label: "Spark",
-            },
-            {
-                id: getRandomId(),
-                img: googleCloudImg,
-                label: "Google Cloud Platform",
-            },
-            {
-                id: getRandomId(),
-                img: hiveImg,
-                label: "Hive",
-            },
-            {
-                id: getRandomId(),
-                img: prestoImg,
-                label: "Presto",
-            },
-            {
-                id: getRandomId(),
                 img: pythonImg,
-                label: "Python",
+                label: "PYTHON",
             },
             {
                 id: getRandomId(),
                 img: scalaImg,
-                label: "Scala",
+                label: "SCALA",
             },
-        ],
+            {
+                id: getRandomId(),
+                img: golangImg,
+                label: "GOLANG",
+            },
+            {
+                id: getRandomId(),
+                img: sparkImg,
+                label: "SPARK",
+            },
+            {
+                id: getRandomId(),
+                img: bigqueryImg,
+                label: "BIGQUERY",
+            },
+            {
+                id: getRandomId(),
+                img: clickhouseImg,
+                label: "CLICKHOUSE",
+            },
+            {
+                id: getRandomId(),
+                img: trinoImg,
+                label: "TRINO",
+            },
+            {
+                id: getRandomId(),
+                img: icebergImg,
+                label: "ICEBERG",
+            },
+            {
+                id: getRandomId(),
+                img: kafkaImg,
+                label: "KAFKA",
+            },
+            {
+                id: getRandomId(),
+                img: airflowImg,
+                label: "AIRFLOW",
+            }
+        ]
+    },
+    {
+        id: getRandomId(),
+        label: "Data Science",
+        lang: [
+            {
+                id: getRandomId(),
+                img: pythonImg,
+                label: "PYTHON",
+            },
+            {
+                id: getRandomId(),
+                img: golangImg,
+                label: "GOLANG",
+            },
+            {
+                id: getRandomId(),
+                img: scipyImg,
+                label: "SCIPY",
+            },
+            {
+                id: getRandomId(),
+                img: scikitLearnImg,
+                label: "SCIKIT-LEARN",
+            },
+            {
+                id: getRandomId(),
+                img: opencvImg,
+                label: "OPENCV",
+            },
+            {
+                id: getRandomId(),
+                img: catboostImg,
+                label: "CATBOOST",
+            },
+            {
+                id: getRandomId(),
+                img: prestoImg,
+                label: "PRESTO",
+            },
+            {
+                id: getRandomId(),
+                img: pytorchImg,
+                label: "PYTORCH",
+            },
+            {
+                id: getRandomId(),
+                img: gcpImg,
+                label: "GCP",
+            },
+            {
+                id: getRandomId(),
+                img: kubeflowImg,
+                label: "KUBEFLOW",
+            }
+        ]
     },
     {
         id: getRandomId(),
@@ -152,29 +371,39 @@ export const sliderData: Array<TSlideData> = [
         lang: [
             {
                 id: getRandomId(),
-                img: kotlinImg,
-                label: "Kotlin",
+                img: qaseImg,
+                label: "QASE",
+            },
+            {
+                id: getRandomId(),
+                img: proxymanImg,
+                label: "PROXYMAN",
+            },
+            {
+                id: getRandomId(),
+                img: postmanImg,
+                label: "POSTMAN",
             },
             {
                 id: getRandomId(),
                 img: mysqlImg,
-                label: "MySQL",
+                label: "MYSQL",
             },
             {
                 id: getRandomId(),
                 img: appiumImg,
-                label: "Appium",
+                label: "APPIUM",
             },
             {
                 id: getRandomId(),
-                img: vueImg,
-                label: "Vue",
+                img: allureImg,
+                label: "ALLURE",
             },
-        ],
-    },
-    {
-        id: getRandomId(),
-        label: "Security",
-        lang: [],
-    },
+            {
+                id: getRandomId(),
+                img: kotlinImg,
+                label: "KOTLIN",
+            }
+        ]
+    }
 ];
